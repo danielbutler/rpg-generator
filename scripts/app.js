845e2fdd791465a4b3f796ec056d6b5b4830ccb4
Daniel Butler <cactusfly@gmail.com>
2016-11-03 18:13:42 -0400

    Base site works with Angular

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,5 @@
-'use strict';
+var myApp = angular.module('myApp',[]);
 
-angular.module("rpgGenerator", [])
-.controller('mainCtrl', function($scope, $http) {
-  dataService.getRPGList(function(response) {
-    console.log(response.data);
-    $scope.todos = response.data;
-  });
-});
-
-.service('dataService', function($http) {
-  this.helloConsole = function() {
-    console.log('This is the helloConsole service!');
-  };
-
-  this.getRPGList = function(callback) {
-    $http.get('./mock/data.json')
-    .then(function (response) {
-        $scope.myWelcome = response.data;
-    });
-});
+myApp.controller('GreetingController', ['$scope', function GreetingController($scope) {
+  $scope.greeting = 'Hola!';
+}]);
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
+'use strict';
 
+var express = require('express');
 var router = express.Router();
 
 module.exports = router;
