6a03962c0371f2169703d1c7cf037b6eff8c333a
Daniel Butler <cactusfly@gmail.com>
2016-11-04 14:54:50 -0400

    angular now reading JSON file and spitting out a string

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,42 @@
 'use strict';
 
+var pcMessage = [
+  "The PCs are ",
+  " ",
+  " ",
+  " who, with ",
+  ", fight ",
+  " for ",
+  " "
+];
+
+function MessageCreator (data) {
+  var pcKeys = [];
+  var message = "";
+  for (var key in data) {
+    pcKeys.push(key);
+  }
+  for (var i = 0; i < pcMessage.length; i ++) {
+    message += pcMessage[i]+data[pcKeys[i]];
+  }
+  return message;
+};
+
 angular.module('rpgGenerator',[])
 .controller('mainCTRL', ['$scope', 'data', function mainCTRL($scope, data) {
   $scope.greeting = 'Hola!';
 
   data.getInfo(function(response) {
-    console.log(response.data);
-    $scope.PcInfo = response.data;
+    // console.log(response.data);
+    $scope.PcInfoRaw = response.data;
+    $scope.PcInfo = MessageCreator($scope.PcInfoRaw);
+    // $scope.PcMessage = {{PcInfo.pcsAreAdjective[0]}}
+    // $scope.PcMessage += {{PcInfo.pcsAre}}
+    // $scope.PcMessage += $scope.PcMessage += $scope.PcMessage += $scope.PcMessage += $scope.PcMessage += {{PcInfo.pcsLocation}}
+    // $scope.PcMessage += $scope.PcMessage += $scope.PcMessage += $scope.PcMessage += {{PcInfo.pcsWhoWith}}
+    // $scope.PcMessage += $scope.PcMessage += $scope.PcMessage += {{PcInfo.pcsFight}}
+    // $scope.PcMessage += $scope.PcMessage += {{PcInfo.pcsFor}}
+    // $scope.PcMessage += {{PcInfo.pcsWhy}}
   });
 
 }])
