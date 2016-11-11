b83240bb64355937afee9f363e6a521c4eeeb528
Daniel Butler <cactusfly@gmail.com>
2016-11-10 21:36:30 -0500

    Submit button now captures data and outputs to array

--- scripts/app.js
+++ scripts/app.js
@@ -50,16 +50,11 @@ function MessageCreator (data, form) {
 
 angular.module('rpgGenerator',[])
 .controller('mainCTRL', ['$scope', 'data', function mainCTRL($scope, data) {
-  $scope.greeting = 'Hola!';
 
   data.getInfo(function(response) {
     $scope.PcInfoRaw = response.data;
     $scope.PcInfo = MessageCreator($scope.PcInfoRaw);
   });
-
-  $scope.numberGenerator = function() {
-    console.log(getRandomInt(0, 7));
-  };
 
   $scope.getPcInfo = function() {
     var form = false;
@@ -71,13 +66,20 @@ angular.module('rpgGenerator',[])
     $scope.PcInput = MessageCreator($scope.PcInfoRaw, form);
   };
 
-  $scope.save = function() {
+  $scope.save = function(data) {
     /*$http.post('path/to/server/file/to/save/json', $scope.languages).then(function(data) {
       $scope.msg = 'Data saved';
     });*/
-    $scope.msg = 'Data sent: '+ JSON.stringify($scope.languages);
-    console.log($scope.msg);
-  };
+
+    var saveFile = [];
+    angular.forEach(data, function(value, key) {
+      if(data[key].edited === true && value.items != null && value.items != "") {
+        console.log(data[key].edited);
+        this.push({"key": key, "value": value.items});
+      }
+    }, saveFile);
+    console.log(saveFile);
+    };
 
 }])
 
