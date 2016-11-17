b124991e88e2cc4213d3d1911c7333337191e742
Daniel Butler <cactusfly@gmail.com>
2016-11-17 17:23:05 -0500

    pretty-fied it!

--- src/app.js
+++ src/app.js
@@ -15,6 +15,6 @@ app.use(parser.json());
 
 app.use('/api', router);
 
-app.listen(3000, function() {
+app.listen(3000, '0.0.0.0', function() {
     console.log("The server is running on port 3000!");
 });
